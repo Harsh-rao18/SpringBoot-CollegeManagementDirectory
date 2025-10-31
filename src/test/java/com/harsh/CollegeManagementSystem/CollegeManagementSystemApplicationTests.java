e698d714dba62ac6395a4a759cc6f5a2dc472324
Harsh-rao18 <harshrao64644@gmail.com>
2025-10-31 19:41:09 +0530

    creating project and adding dependency

---
+++ src/test/java/com/harsh/CollegeManagementSystem/CollegeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.CollegeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
